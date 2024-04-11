a5e5dec175a26462d684434514ba21a81f5eb940
jphaugla <jasonhaugland@gmail.com>
2024-04-11 18:54:33 -0500

    fix generateLots and startAppServers for larger throughput

--- scripts/generateLots.sh
+++ scripts/generateLots.sh
@@ -1,8 +1,8 @@
-# for server testing to generate higher load levels.  
-# Use with startAppservers.sh 
-nohup curl 'http://localhost:8080/generateData?noOfCustomers=5000&noOfTransactions=100000&noOfDays=10&key_suffix=J' > /tmp/generateJ.out 2>&1 &
-nohup curl 'http://localhost:8081/generateData?noOfCustomers=5000&noOfTransactions=100000&noOfDays=10&key_suffix=P' > /tmp/generateP.out 2>&1 &
-nohup curl 'http://localhost:8082/generateData?noOfCustomers=5000&noOfTransactions=100000&noOfDays=10&key_suffix=H' > /tmp/generateH.out 2>&1 &
-nohup curl 'http://localhost:8083/generateData?noOfCustomers=5000&noOfTransactions=100000&noOfDays=10&key_suffix=C' > /tmp/generateC.out 2>&1 &
-nohup curl 'http://localhost:8084/generateData?noOfCustomers=5000&noOfTransactions=100000&noOfDays=10&key_suffix=A' > /tmp/generateA.out 2>&1 &
-nohup curl 'http://localhost:8085/generateData?noOfCustomers=5000&noOfTransactions=100000&noOfDays=10&key_suffix=G' > /tmp/generateG.out 2>&1 &
+# for server testing to generate higher load levels.
+# Use with startAppservers.sh
+nohup curl 'http://localhost:8080/api/transaction/generateData?noOfCustomers=5000&noOfTransactions=100000&noOfDays=10&key_suffix=J&doKafka=false' > /tmp/generateJ.out 2>&1 &
+nohup curl 'http://localhost:8081/api/transaction/generateData?noOfCustomers=5000&noOfTransactions=100000&noOfDays=10&key_suffix=P&doKafka=false' > /tmp/generateP.out 2>&1 &
+nohup curl 'http://localhost:8082/api/transaction/generateData?noOfCustomers=5000&noOfTransactions=100000&noOfDays=10&key_suffix=H&doKafka=false' > /tmp/generateH.out 2>&1 &
+nohup curl 'http://localhost:8083/api/transaction/generateData?noOfCustomers=5000&noOfTransactions=100000&noOfDays=10&key_suffix=C&doKafka=false' > /tmp/generateC.out 2>&1 &
+nohup curl 'http://localhost:8084/api/transaction/generateData?noOfCustomers=5000&noOfTransactions=100000&noOfDays=10&key_suffix=A&doKafka=false' > /tmp/generateA.out 2>&1 &
+nohup curl 'http://localhost:8085/api/transaction/generateData?noOfCustomers=5000&noOfTransactions=100000&noOfDays=10&key_suffix=G&doKafka=false' > /tmp/generateG.out 2>&1 &

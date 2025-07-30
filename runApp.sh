bc69c3d1c6f2e82651e34c44e35b5a8a3508ce7c
jphaugla <jasonhaugland@gmail.com>
2025-07-30 08:16:38 -0500

    add bash header to runAopp.sh

--- runApp.sh
+++ runApp.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 source scripts/setEnv.sh; java -jar target/cockroach-0.0.1-SNAPSHOT.jar  --spring.datasource.url="${COCKROACH_URL}" --spring.datasource.username="${COCKROACH_DB_USER}" --spring.datasource.password="${COCKROACH_DB_PASS}"
